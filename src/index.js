2ffcd0a753f9ef62f92a1713d13d2723b33f985f
nomad <noneprado@gmail.com>
2019-10-13 23:36:54 +0600

    hot fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import App from './components/App'
 import { actionCreators } from './redux/actions'
 import Store from './redux/store'
+import * as serviceWorker from './registerServiceWorker';
 
 const store = Store();
 store.dispatch(actionCreators.getAll());
@@ -14,3 +15,9 @@ render(
     </Provider>,
     document.getElementById('root')
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();

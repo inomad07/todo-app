68b261082ef92f404d67b14b42bc16195a102205
nomad <noneprado@gmail.com>
2019-10-13 23:49:24 +0600

    hot fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom'
 import { Provider } from 'react-redux';
-import './css/style.css';
+
 import App from './containers/app'
 import todoReducer from './redux/reducers'
 import Store from "./redux/store";
+import './css/style.css';
+import * as serviceWorker from './registerServiceWorker';
 
 const store = Store(todoReducer);
 
@@ -14,3 +16,9 @@ render(
     </Provider>,
     document.getElementById('root')
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();

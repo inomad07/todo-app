3c7a83c8c4417e7d27e46ab180d78b54936c305c
nomad <noneprado@gmail.com>
2019-12-21 16:58:02 +0600

    Added title.css

--- src/common/components/Title/Title.js
+++ src/common/components/Title/Title.js
@@ -1,27 +1,16 @@
 import React from "react";
 
-const styles = {
-    header: {
-        backgroundColor: "skyblue",
-        padding: 15
-    },
-    title: {
-        textAlign: "center",
-        color: "white"
-    }
-};
-
+import './title.css';
 
 const Title = (props) => {
     const { children } = props;
 
     return (
-        <div style={styles.header}>
-            <div style={styles.title}>{children}</div>
+        <div className="header">
+            <div className="title">{children}</div>
         </div>
     );
 };
-
 
 
 export default Title;
